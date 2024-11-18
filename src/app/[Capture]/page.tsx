c28bcd1095af32aa991207cfe62e1cb8bca40277
PRITAM SAHA <pritamstech@hotmail.com>
2024-11-18 21:18:40 +0530

    Maybe The Debugging Comes To An End

--- src/app/[Capture]/page.tsx
+++ src/app/[Capture]/page.tsx
@@ -12,8 +12,6 @@ const CapturePage = async (path: { params: Promise<{ Capture: string }> }) => {
     const Link = await Links.findOne({
       ShortUrl: `${process.env.NEXT_PUBLIC_HOST}/${params.Capture}`,
     });
-    // console.log(Link);
-    console.log(`${process.env.NEXT_PUBLIC_HOST}/${params.Capture}`);
 
     if (Link) {
       const MyUser = await User.findOne({ _id: Link.UserID });
@@ -26,19 +24,13 @@ const CapturePage = async (path: { params: Promise<{ Capture: string }> }) => {
       await Links.findByIdAndUpdate(Link._id, {
         $inc: { Clicks: 1 },
       });
-      console.log(Link);
-      console.log(Link.OriginalUrl);
-
-      window.location.href = Link.OriginalUrl;
-      return;
+      return redirect(Link.OriginalUrl);
     }
     return redirect(String(process.env.NEXT_PUBLIC_HOST));
   } catch (error) {
     if (error instanceof Error) {
       return redirect(String(process.env.NEXT_PUBLIC_HOST));
     }
-  } finally {
-    return redirect(String(process.env.NEXT_PUBLIC_HOST));
   }
 };
 
