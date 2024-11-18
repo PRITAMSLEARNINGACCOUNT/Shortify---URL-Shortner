606de4f5545488f3097f664a30281ec62040f7dc
PRITAM SAHA <pritamstech@hotmail.com>
2024-11-18 20:58:00 +0530

    A Small Bug Fix

--- src/app/[Capture]/page.tsx
+++ src/app/[Capture]/page.tsx
@@ -12,6 +12,7 @@ const CapturePage = async (path: { params: Promise<{ Capture: string }> }) => {
     const Link = await Links.findOne({
       ShortUrl: `${process.env.NEXT_PUBLIC_HOST}/${params.Capture}`,
     });
+    console.log(Link);
 
     if (Link) {
       const MyUser = await User.findOne({ _id: Link.UserID });
@@ -24,12 +25,12 @@ const CapturePage = async (path: { params: Promise<{ Capture: string }> }) => {
       await Links.findByIdAndUpdate(Link._id, {
         $inc: { Clicks: 1 },
       });
-      redirect(Link.OriginalUrl);
+      return redirect(Link.OriginalUrl);
     }
     return redirect(String(process.env.NEXT_PUBLIC_HOST));
   } catch (error) {
     if (error instanceof Error) {
-      redirect(String(process.env.NEXT_PUBLIC_HOST));
+      return redirect(String(process.env.NEXT_PUBLIC_HOST));
     }
   } finally {
     return redirect(String(process.env.NEXT_PUBLIC_HOST));
